6266fca65f781b6daa3f6c4aa7a91fcd386cfee4
Zia Ur Rehman <75566431+ziaurrehman442@users.noreply.github.com>
2023-09-16 21:25:28 +0500

    d

--- frontend/src/templets.js
+++ frontend/src/templets.js
@@ -13,34 +13,34 @@ function Templates(props){
     if(temp!==''){
       return(
         <div className="container">
-          <div class="card-group">
+          <div className="card-group">
           {
             temp.map(element => {
               return(
-                <div class="card template">
-                <img src={"uploads/"+element.image} class="card-img-top" alt="..."/>
-                <div class="card-body">
-                    <h5 class="card-title">Card title</h5>
-                    <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                <div className="card template" key={element.T_id}>
+                <img src={"uploads/"+element.image} className="card-img-top card-1" alt="..."/>
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target={"#exampleModal"+element.T_id}>
                       Preview
                     </button>
-                    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                      <div class="modal-dialog">
-                        <div class="modal-content">
-                          <div class="modal-header">
-                            <h5 class="modal-title" id="exampleModalLabel">Template1</h5>
-                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div className="modal fade" id={"exampleModal"+element.T_id} tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                      <div className="modal-dialog">
+                        <div className="modal-content">
+                          <div className="modal-header">
+                            <h5 className="modal-title" id="exampleModalLabel">{element.T_id}</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                           </div>
-                          <div class="modal-body">
-                            <img src="uploads/cv.jpg" class="card-img-top" alt="..."/>
+                          <div className="modal-body">
+                            <img src={"uploads/"+element.image} className="card-img-top" alt="..."/>
                           </div>
-                          <div class="modal-footer">
-                            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                          <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
                           </div>
                         </div>
                       </div>
                     </div>
-                    <button onClick={props.onSet()} class="btn btn-primary mx-3">SELECT</button>
+                    <button onClick={props.onSet(element.T_id)} className="btn btn-primary mx-3">SELECT</button>
                 </div>
             </div> 
         
